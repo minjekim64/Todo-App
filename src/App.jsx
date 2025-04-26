5b06cc29df58880735dc17c9ad3f40a6c56c5248
dltmfql456 <dltmfql456@naver.com>
2025-04-26 17:34:19 +0900

    Todo 생성 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todo, setTodo] = useState([
+    {id: Date.now(), content: '안녕하세요'}
+  ]);
+  const inputRef = useRef(null);
+
+  const addTodoHandler = () => {
+    const newTodo = {
+      id: Date.now(), content: inputRef.current.value
+    };
+    setTodo(prev => [...prev, newTodo]);
+  }
 
   return (
     <>
-
+      <input type="text" ref={inputRef}/>
+      <button onClick={addTodoHandler}>추가</button>
+      <ul>
+        {todo.map((el) => 
+          <li key={el.id}>{el.content}</li>
+        )}
+      </ul>
     </>
   )
 }
